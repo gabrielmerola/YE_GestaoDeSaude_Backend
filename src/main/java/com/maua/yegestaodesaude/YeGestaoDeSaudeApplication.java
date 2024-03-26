135bdd3aa3f261704c665a5ccb52a17dca382f17
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-26 16:38:37 -0300

    initial project

---
+++ src/main/java/com/maua/yegestaodesaude/YeGestaoDeSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.maua.yegestaodesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeGestaoDeSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeGestaoDeSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maua/yegestaodesaude/YeGestaoDeSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maua.yegestaodesaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeGestaoDeSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
