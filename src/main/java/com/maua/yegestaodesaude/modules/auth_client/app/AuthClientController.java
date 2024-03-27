44541393512413107a61dd1470a5d107777f98cb
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-26 23:00:19 -0300

    adding auth controller

---
+++ src/main/java/com/maua/yegestaodesaude/modules/auth_client/app/AuthClientController.java
@@ -0,0 +1,27 @@
+package com.maua.yegestaodesaude.modules.auth_client.app;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maua.yegestaodesaude.shared.domain.dtos.AuthDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthClientController {
+    
+    @Autowired
+    private AuthClientUsecase authClientUsecase;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String authClient(@RequestBody AuthDto authdto){
+        var result = this.authClientUsecase.execute(authdto);
+        return result;
+    }
+}
