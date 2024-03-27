86f0d25462aaa421d4159ead6e082d445832a73a
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-26 23:02:00 -0300

    adding Client Entity

---
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/entities/Client.java
@@ -0,0 +1,73 @@
+package com.maua.yegestaodesaude.shared.domain.entities;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "client")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Client implements UserDetails {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public Client(String name, String email, String password, String phone) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
