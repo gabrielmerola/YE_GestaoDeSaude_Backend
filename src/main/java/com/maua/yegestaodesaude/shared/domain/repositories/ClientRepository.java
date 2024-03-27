72e4119f55eb99f4253216f1ca05ae9aec345ae2
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-26 23:01:47 -0300

    adding client repository

---
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.maua.yegestaodesaude.shared.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maua.yegestaodesaude.shared.domain.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+    Client findByEmail(String email);
+}
