de25c54012331fcdb33bfd1f66faf53bc8715bcc
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-26 23:01:14 -0300

    adding CreateClient usecase

--- src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
@@ -1,2 +1,37 @@
-package com.maua.yegestaodesaude.modules.create_client.app;public class CreateClientUsecase {
+package com.maua.yegestaodesaude.modules.create_client.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.maua.yegestaodesaude.shared.domain.dtos.ClientDto;
+import com.maua.yegestaodesaude.shared.domain.entities.Client;
+import com.maua.yegestaodesaude.shared.domain.repositories.ClientRepository;
+
+@Service
+public class CreateClientUsecase {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ResponseEntity<Object> execute(ClientDto clientDto){
+
+        Client clientExists = clientRepository.findByEmail(clientDto.email());
+
+        if(clientExists != null){
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body("Client already exists");
+        }
+
+        var passwordCrypted = passwordEncoder.encode(clientDto.password());
+
+        Client entity = new Client(clientDto.name(), clientDto.email(), passwordCrypted, clientDto.phone());
+        Client newClient = clientRepository.save(entity);
+        
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(new ClientDto(newClient.getName(), newClient.getEmail(), newClient.getPassword(), newClient.getPhone()));
+    }
 }
