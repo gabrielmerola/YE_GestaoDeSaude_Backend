a7dbf386408c1d50d20484f65532ee5df524d54e
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-27 15:04:36 -0300

    adding registration field validations before saving to the database

--- src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
@@ -23,9 +23,18 @@ public class CreateClientUsecase {
     public ResponseEntity<Object> execute(ClientDto clientDto){
 
         Client clientExists = clientRepository.findByEmail(clientDto.email());
-
         if(clientExists != null){
-            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(new ClientFoundException("Usuário ja cadastrado"));
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(new ClientFoundException("Email ja cadastrado"));
+        }
+
+        clientExists = clientRepository.findByCpf(clientDto.cpf());
+        if(clientExists != null){
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(new ClientFoundException("CPF ja cadastrado"));
+        }
+
+        clientExists = clientRepository.findByPhone(clientDto.phone());
+        if(clientExists != null){
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(new ClientFoundException("Telefone ja cadastrado"));
         }
 
         var passwordCrypted = passwordEncoder.encode(clientDto.password());
@@ -34,6 +43,18 @@ public class CreateClientUsecase {
 
         if(!entity.validateCPF(clientDto.cpf())){
             return ResponseEntity.status(HttpStatusCode.valueOf(422)).body("CPF inválido");
+        }
+        if(!entity.validateEmail(clientDto.email())){
+            return ResponseEntity.status(HttpStatusCode.valueOf(422)).body("Email inválido");
+        }
+        if(!entity.validatePassword(clientDto.password())){
+            return ResponseEntity.status(HttpStatusCode.valueOf(422)).body("Senha inválida");
+        }
+        if(clientDto.name() == null || clientDto.name().isEmpty() || clientDto.name().length() < 3){
+            return ResponseEntity.status(HttpStatusCode.valueOf(422)).body("Nome inválido");
+        }
+        if(clientDto.phone() == null || clientDto.phone().isEmpty() || clientDto.phone().length() < 11){
+            return ResponseEntity.status(HttpStatusCode.valueOf(422)).body("Telefone inválido");
         }
 
         clientRepository.save(entity);
