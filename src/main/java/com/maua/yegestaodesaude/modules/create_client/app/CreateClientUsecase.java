e3e8c12045c0192d0e3db30fa52ccbf1612753d0
Gabriel Merola <gabrielmerola12@gmail.com>
2024-03-27 10:02:15 -0300

    Ajusting return body createClient

--- src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/create_client/app/CreateClientUsecase.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.maua.yegestaodesaude.shared.domain.dtos.ClientDto;
 import com.maua.yegestaodesaude.shared.domain.entities.Client;
 import com.maua.yegestaodesaude.shared.domain.repositories.ClientRepository;
+import com.maua.yegestaodesaude.shared.helpers.exceptions.ClientFoundException;
 
 @Service
 public class CreateClientUsecase {
@@ -24,14 +25,14 @@ public class CreateClientUsecase {
         Client clientExists = clientRepository.findByEmail(clientDto.email());
 
         if(clientExists != null){
-            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body("Client already exists");
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(new ClientFoundException("Usuário ja cadastrado"));
         }
 
         var passwordCrypted = passwordEncoder.encode(clientDto.password());
 
-        Client entity = new Client(clientDto.name(), clientDto.email(), passwordCrypted, clientDto.phone());
+        Client entity = new Client(clientDto.name(), clientDto.email(), passwordCrypted, clientDto.phone(), clientDto.cpf());
         Client newClient = clientRepository.save(entity);
         
-        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(new ClientDto(newClient.getName(), newClient.getEmail(), newClient.getPassword(), newClient.getPhone()));
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(new ClientDto(newClient.getName(), newClient.getEmail(), newClient.getPassword(), newClient.getPhone(), newClient.getCpf()));
     }
 }
