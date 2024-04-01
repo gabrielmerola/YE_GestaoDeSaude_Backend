4d784cc4cff9c54d8c059f7380b27578431a1d63
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-01 15:46:20 -0300

    Create GetBloodPressureViewmodel

---
+++ src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure/app/GetBloodPressureViewmodel.java
@@ -0,0 +1,19 @@
+package com.maua.yegestaodesaude.modules.bloodPressure.get_blood_pressure.app;
+
+import java.sql.Date;
+
+import com.maua.yegestaodesaude.shared.domain.entities.BloodPressure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class GetBloodPressureViewmodel {
+    private Long id;
+    private Date date;
+    private String measure;
+    private String level;
+}
