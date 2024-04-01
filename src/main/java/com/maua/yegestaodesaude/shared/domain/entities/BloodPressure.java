5a245bbd66281264c3d4e679d56b647c6aa59946
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-01 14:25:03 -0300

    adding levelPressure function

--- src/main/java/com/maua/yegestaodesaude/shared/domain/entities/BloodPressure.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/entities/BloodPressure.java
@@ -24,4 +24,21 @@ public class BloodPressure {
     private Date date;
     private String measure;
     private String level;
+
+    public String levelPressure(String measure){
+        String[] measures = measure.split("x");
+        int systolic = Integer.parseInt(measures[0]);
+        int diastolic = Integer.parseInt(measures[1]);
+        if(systolic < 90 && diastolic < 60){
+            return "Baixa";
+        } else if(systolic < 120 && diastolic < 80){
+            return "Ótima";
+        } else if((systolic > 120 && systolic < 129) && (diastolic > 80 && diastolic < 84)){
+            return "Normal";
+        } else if((systolic > 130 && systolic < 139) && (diastolic > 85 && diastolic < 89)){
+            return "Atenção";
+        } else {
+            return "Alta";
+        }
+    }
 }
