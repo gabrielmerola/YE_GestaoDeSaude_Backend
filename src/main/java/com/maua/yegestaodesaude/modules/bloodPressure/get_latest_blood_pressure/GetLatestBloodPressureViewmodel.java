af96a7ceae8aa751af89628e3d5550687337ba87
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-01 17:08:45 -0300

    create Viewmodel GetLatestBloodPressure

---
+++ src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_latest_blood_pressure/GetLatestBloodPressureViewmodel.java
@@ -0,0 +1,17 @@
+package com.maua.yegestaodesaude.modules.bloodPressure.get_latest_blood_pressure;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class GetLatestBloodPressureViewmodel {
+    private Long id;
+    private Date date;
+    private String measure;
+    private String level;
+}
