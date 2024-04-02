562506bc06d1bec4161b874ed2232ad1be0c608f
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:53:06 -0300

    create DeleteGlucoseUsecase

---
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/delete_glucose/app/DeleteGlucoseUsecase.java
@@ -0,0 +1,12 @@
+package com.maua.yegestaodesaude.modules.glucose.delete_glucose.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteGlucoseUsecase {
+    
+    public ResponseEntity<Object> execute() {
+        return null;
+    }
+}
