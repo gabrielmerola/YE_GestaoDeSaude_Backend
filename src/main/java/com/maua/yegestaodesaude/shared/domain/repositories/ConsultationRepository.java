bbbadc3f105e8f7212cd4bad3b9a7fc4c3283c8f
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-02 10:15:32 -0300

    adding method to interface

--- src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/ConsultationRepository.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/ConsultationRepository.java
@@ -1,9 +1,11 @@
 package com.maua.yegestaodesaude.shared.domain.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.maua.yegestaodesaude.shared.domain.entities.Consultation;
 
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-
+    List<Consultation> findAllByClientId(Long clientId);
 }
