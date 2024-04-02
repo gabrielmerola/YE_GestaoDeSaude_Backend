b6cdabb3416b29fb664d423e4495e13b5a944276
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:57:34 -0300

    adding RequestParam

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/delete_glucose/app/DeleteGlucoseController.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/delete_glucose/app/DeleteGlucoseController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -53,9 +54,9 @@ public class DeleteGlucoseController {
             }
         )
     })
-    public ResponseEntity<Object> deleteGlucose() {
+    public ResponseEntity<Object> deleteGlucose(@RequestParam Long id) {
         try {
-            var result = deleteGlucoseUsecase.execute();
+            var result = deleteGlucoseUsecase.execute(id);
             return result;
         } catch (Exception e) {
             return ResponseEntity.status(400).body("{\"message\": \"" + e.getMessage() + "\"}");
