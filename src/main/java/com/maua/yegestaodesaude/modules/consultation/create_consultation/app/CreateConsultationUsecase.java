3a5f2c3bc107e031143a26d2fd20909968982335
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-02 10:50:36 -0300

    comments

--- src/main/java/com/maua/yegestaodesaude/modules/consultation/create_consultation/app/CreateConsultationUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/consultation/create_consultation/app/CreateConsultationUsecase.java
@@ -22,16 +22,13 @@ public class CreateConsultationUsecase {
 
     public ResponseEntity<Object> execute(ConsultationDTO consultationDTO, Long clientId) {
         try {
-            // Verifica se o cliente existe
             Client client = this.clientRepository.findById(clientId).orElseThrow(() -> {
                 throw new RuntimeException("Cliente não encontrado!");
             });
 
-            // Converte as datas do DTO para o formato SQL Date
             Date sqlDate = Date.valueOf(consultationDTO.date());
             Date sqlDateReturn = Date.valueOf(consultationDTO.dateReturn());
 
-            // Cria uma nova consulta
             Consultation consultation = new Consultation();
             consultation.setClient(client);
             consultation.setName(consultationDTO.name());
@@ -40,10 +37,8 @@ public class CreateConsultationUsecase {
             consultation.setDateReturn(sqlDateReturn);
             consultation.setDescription(consultationDTO.description());
 
-            // Salva a consulta no repositório
             consultation = consultationRepository.save(consultation);
 
-            // Retorna a resposta
             return ResponseEntity.status(201).body(new CreateConsultationViewmodel("Consulta criada com sucesso"));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new CreateConsultationViewmodel(e.getMessage()));
