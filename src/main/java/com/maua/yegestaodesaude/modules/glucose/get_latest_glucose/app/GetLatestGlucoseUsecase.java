9b0f11dedc933ef68f2047b75a419e4d55eb43ac
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:44:37 -0300

    adding GetLatestGlucoseUsecase

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseUsecase.java
@@ -1,12 +1,34 @@
 package com.maua.yegestaodesaude.modules.glucose.get_latest_glucose.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetLatestGlucoseUsecase {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     
-    public ResponseEntity<Object> execute(){
-        return ResponseEntity.status(200).body("ok");
+    public ResponseEntity<Object> execute(Long clientId){
+        var result = findLastItemInserted(clientId);
+        if(result == null){
+            return ResponseEntity.status(204).body("{\"message\": \"Sem conteúdo\"}");
+        }
+        return ResponseEntity.status(200).body(result);
+    }
+
+    private Object findLastItemInserted(Long clientId){
+        String sql = "SELECT * FROM glucose WHERE client_id = " + clientId + " ORDER BY date DESC LIMIT 1";
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+            GetLatestGlucoseViewmodel item = new GetLatestGlucoseViewmodel(
+                rs.getLong("id"),
+                rs.getDate("date"),
+                rs.getString("measure"),
+                rs.getString("level")
+            );
+            item.setId(rs.getLong("id"));
+            return item;
+        });
     }
 }
