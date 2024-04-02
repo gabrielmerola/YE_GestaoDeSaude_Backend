c3c66258d7533952437dc24f6078d1f697188476
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:29:11 -0300

    adding GetGlucoseViewmodel

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose/app/GetGlucoseViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose/app/GetGlucoseViewmodel.java
@@ -1,5 +1,17 @@
 package com.maua.yegestaodesaude.modules.glucose.get_glucose.app;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class GetGlucoseViewmodel {
-    
+    private Long id;
+    private Date date;
+    private Integer measure;
+    private String level;
 }
