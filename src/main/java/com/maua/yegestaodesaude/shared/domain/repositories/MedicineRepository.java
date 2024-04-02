00795c58b4dcfa8f062c94e25c270918d37c9d41
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 11:39:12 -0300

    Create MedicineRepository

---
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/MedicineRepository.java
@@ -0,0 +1,10 @@
+package com.maua.yegestaodesaude.shared.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maua.yegestaodesaude.shared.domain.entities.Medicine;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+}
