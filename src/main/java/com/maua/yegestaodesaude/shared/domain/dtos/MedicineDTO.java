17996deed01757eaa17d02d189321a461bed8c1e
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 12:10:50 -0300

    adding swagger example

--- src/main/java/com/maua/yegestaodesaude/shared/domain/dtos/MedicineDTO.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/dtos/MedicineDTO.java
@@ -1,9 +1,17 @@
 package com.maua.yegestaodesaude.shared.domain.dtos;
 
-public class MedicineDTO {
-    String name;
-    String time;
-    Integer period;
-    Integer quantity;
-    String dosage;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MedicineDTO (
+    @Schema(example = "Dipirona")
+    String name,
+    @Schema(example = "08:00:00")
+    String time,
+    @Schema(example = "6")
+    Integer period,
+    @Schema(example = "2")
+    Integer quantity,
+    @Schema(example = "1 comprimido de 500mg")
+    String dosage
+){
 }
