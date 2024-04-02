0eb592e9124f0b691bbfa355c773ee1d07de7379
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 02:25:25 -0300

    adjusting swagger documentation in client authentication route

--- src/main/java/com/maua/yegestaodesaude/modules/client/auth_client/app/AuthClientController.java
+++ src/main/java/com/maua/yegestaodesaude/modules/client/auth_client/app/AuthClientController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.maua.yegestaodesaude.shared.domain.dtos.AuthDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,25 @@ public class AuthClientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Autenticação de cliente")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Cliente autenticado com sucesso",
+            content = { @io.swagger.v3.oas.annotations.media.Content(
+                mediaType = "application/json",
+                schema = @io.swagger.v3.oas.annotations.media.Schema(example = "{\"token\": \"token\"}")
+            )}
+        ),
+        @ApiResponse(
+            responseCode = "400", 
+            description = "Erro na autenticação do cliente",
+            content = { @io.swagger.v3.oas.annotations.media.Content(
+                mediaType = "application/json",
+                schema = @io.swagger.v3.oas.annotations.media.Schema(example = "{\"error\": \"Erro na autenticação do cliente\"}")
+            )}
+        )
+    })
     public AuthClientViewmodel authClient(@RequestBody AuthDto authdto) {
         String token = this.authClientUsecase.execute(authdto);
         return new AuthClientViewmodel(token);
--- src/main/java/com/maua/yegestaodesaude/shared/domain/dtos/AuthDto.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/dtos/AuthDto.java
@@ -1,7 +1,11 @@
 package com.maua.yegestaodesaude.shared.domain.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AuthDto(
-    String email, 
+    @Schema(example = "exemplo@gmail.com")
+    String email,
+    @Schema(example = "Teste@01")
     String password
 ) {
 }
