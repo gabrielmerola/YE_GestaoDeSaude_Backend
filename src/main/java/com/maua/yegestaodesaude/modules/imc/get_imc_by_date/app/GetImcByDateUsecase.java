5a26bdfdf5a8f09e1438f4867148dffe776cbeeb
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 09:05:33 -0300

    Create GetImcByDateUsecase

---
+++ src/main/java/com/maua/yegestaodesaude/modules/imc/get_imc_by_date/app/GetImcByDateUsecase.java
@@ -0,0 +1,39 @@
+package com.maua.yegestaodesaude.modules.imc.get_imc_by_date.app;
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetImcByDateUsecase {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    public ResponseEntity<Object> execute(Long clientId, String date){
+        Date sqlDate = Date.valueOf(date);
+        var result = findLastItemInserted(clientId, sqlDate);
+        if(result == null){
+            return ResponseEntity.status(204).body("{\"message\": \"Sem conteúdo\"}");
+        }
+        return ResponseEntity.status(200).body(result);
+    }
+
+    private Object findLastItemInserted(Long clientId, Date date){
+        String sql = "SELECT * FROM imc WHERE client_id = " + clientId +" AND date = '" + date + "' LIMIT 1";
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+            GetImcByDateViewmodel item = new GetImcByDateViewmodel(
+                rs.getLong("id"),
+                rs.getDouble("weight"),
+                rs.getDouble("height"),
+                rs.getDouble("imc"),
+                rs.getString("level"),
+                rs.getString("date")
+            );
+            item.setId(rs.getLong("id"));
+            return item;
+        });
+    }
+}
