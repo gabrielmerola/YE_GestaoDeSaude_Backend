45b4da40822f5c77ce1ed171e95737244b9cdf06
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 11:38:51 -0300

    create MedicineDTO - request

---
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/dtos/MedicineDTO.java
@@ -0,0 +1,9 @@
+package com.maua.yegestaodesaude.shared.domain.dtos;
+
+public class MedicineDTO {
+    String name;
+    String time;
+    Integer period;
+    Integer quantity;
+    String dosage;
+}
