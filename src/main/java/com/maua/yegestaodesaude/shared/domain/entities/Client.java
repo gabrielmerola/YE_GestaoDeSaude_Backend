53b71f6ac34e011bc2672f438041f2d2c7da2c02
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 04:14:44 -0300

    remove imports

--- src/main/java/com/maua/yegestaodesaude/shared/domain/entities/Client.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/entities/Client.java
@@ -1,7 +1,6 @@
 package com.maua.yegestaodesaude.shared.domain.entities;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,7 +9,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
