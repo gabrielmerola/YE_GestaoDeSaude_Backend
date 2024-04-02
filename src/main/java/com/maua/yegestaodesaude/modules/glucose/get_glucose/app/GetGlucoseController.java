1efd2b2f881d23a431844ddcaf58b1fc00b2f3fc
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:28:29 -0300

    fixing bugs

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose/app/GetGlucoseController.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose/app/GetGlucoseController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
