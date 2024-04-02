f191df5eab6e96e4139ce2b5c9f9d68e8a57d4ab
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 05:02:59 -0300

    adding CreateImcUsecase

--- src/main/java/com/maua/yegestaodesaude/modules/imc/create_imc/app/CreateImcUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/imc/create_imc/app/CreateImcUsecase.java
@@ -1,14 +1,51 @@
 package com.maua.yegestaodesaude.modules.imc.create_imc.app;
 
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.maua.yegestaodesaude.shared.domain.dtos.ImcDTO;
+import com.maua.yegestaodesaude.shared.domain.entities.Client;
+import com.maua.yegestaodesaude.shared.domain.entities.Imc;
+import com.maua.yegestaodesaude.shared.domain.repositories.ClientRepository;
+import com.maua.yegestaodesaude.shared.domain.repositories.ImcRepository;
 
 @Service
 public class CreateImcUsecase {
     
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private ImcRepository imcRepository;
+
     public ResponseEntity<Object> execute(ImcDTO imcDTO, Long clientId) {
-        return ResponseEntity.ok("oi");
+        Client client = this.clientRepository.findById(clientId).orElseThrow(() -> {
+            throw new RuntimeException("Cliente não encontrado!");
+        });
+
+        String date = imcDTO.date();
+        Date sqlDate = Date.valueOf(date);
+        Imc imc = new Imc();
+
+        imc.setClient(client);
+        imc.setDate(sqlDate);
+        imc.setHeight(imcDTO.height());
+        imc.setWeight(imcDTO.weight());
+
+        Double imcValue = imcDTO.weight() / (imcDTO.height() * imcDTO.height());
+        imc.setImc(imcValue);
+
+        String level = imc.ImcLevel(imcValue);
+        imc.setLevel(level);
+
+        this.imcRepository.save(imc);
+
+        if(imc.getId() == null){
+            return ResponseEntity.status(400).body(new CreateImcViewmodel("Erro ao salvar IMC!"));
+        }
+        return ResponseEntity.status(201).body(new CreateImcViewmodel("IMC criado com sucesso!"));
     }
 }
