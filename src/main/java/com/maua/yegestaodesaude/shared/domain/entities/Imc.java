2ff0ff193a8fc07a1ad617ef1b49724a579cff06
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 05:02:10 -0300

    adding imcLevel

--- src/main/java/com/maua/yegestaodesaude/shared/domain/entities/Imc.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/entities/Imc.java
@@ -26,4 +26,20 @@ public class Imc {
     private Double imc;
     private String level;
     private Date date;
+
+    public String ImcLevel(Double imc){
+        if(imc < 18.5){
+            return "Abaixo do peso";
+        } else if(imc >= 18.5 && imc < 24.9){
+            return "Peso normal";
+        } else if(imc >= 25 && imc < 29.9){
+            return "Sobrepeso";
+        } else if(imc >= 30 && imc < 34.9){
+            return "Obesidade grau 1";
+        } else if(imc >= 35 && imc < 39.9){
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grave";
+        }
+    }
 }
