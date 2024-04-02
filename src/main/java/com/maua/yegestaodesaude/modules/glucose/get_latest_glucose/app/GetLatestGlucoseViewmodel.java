dc1a47943e30eac9ed969bd6b3ddc58bf7ca5a5b
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:44:44 -0300

    adding GetLatestGlucoseViewmodel

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseViewmodel.java
@@ -1,5 +1,17 @@
 package com.maua.yegestaodesaude.modules.glucose.get_latest_glucose.app;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class GetLatestGlucoseViewmodel {
-    
+    private Long id;
+    private Date date;
+    private String measure;
+    private String level;
 }
