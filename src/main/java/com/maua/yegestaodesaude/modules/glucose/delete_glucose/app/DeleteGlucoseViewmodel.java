bdd38c8acf8e0b030653908093299ede05c70ea8
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-02 03:58:18 -0300

    adding format responseMessage

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/delete_glucose/app/DeleteGlucoseViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/delete_glucose/app/DeleteGlucoseViewmodel.java
@@ -1,5 +1,10 @@
 package com.maua.yegestaodesaude.modules.glucose.delete_glucose.app;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class DeleteGlucoseViewmodel {
-    
+    private String message;
 }
