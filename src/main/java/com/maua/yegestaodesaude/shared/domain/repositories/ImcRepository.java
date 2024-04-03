7c4d5858efe01c83eb3bdce2b1a6559801c4ba22
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-03 12:27:31 -0300

    adding method to IImc interface✅

--- src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/ImcRepository.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/ImcRepository.java
@@ -1,5 +1,7 @@
 package com.maua.yegestaodesaude.shared.domain.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.maua.yegestaodesaude.shared.domain.entities.Imc;
 
 @Repository
 public interface ImcRepository extends JpaRepository<Imc, Long> {
-    
+    List<Imc> findAllByClientId(Long clientId);
 }
