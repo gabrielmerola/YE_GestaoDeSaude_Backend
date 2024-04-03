62cc220d5c7722d0d3f98fd8dd510326dda41858
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-03 11:04:39 -0300

    Adding findAllByClientId to MedicineRepository

--- src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/MedicineRepository.java
+++ src/main/java/com/maua/yegestaodesaude/shared/domain/repositories/MedicineRepository.java
@@ -1,5 +1,7 @@
 package com.maua.yegestaodesaude.shared.domain.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.maua.yegestaodesaude.shared.domain.entities.Medicine;
 
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+    List<Medicine> findAllByClientId(Long clientId);
 }
