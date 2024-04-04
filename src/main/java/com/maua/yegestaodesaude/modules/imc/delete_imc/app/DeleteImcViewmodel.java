a76f07533445b0d318c47ee7528e1a7906a425fc
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-04 07:58:21 -0300

    Create DeleteImcViewmodel - response

---
+++ src/main/java/com/maua/yegestaodesaude/modules/imc/delete_imc/app/DeleteImcViewmodel.java
@@ -0,0 +1,10 @@
+package com.maua.yegestaodesaude.modules.imc.delete_imc.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeleteImcViewmodel {
+    private String message;
+}
