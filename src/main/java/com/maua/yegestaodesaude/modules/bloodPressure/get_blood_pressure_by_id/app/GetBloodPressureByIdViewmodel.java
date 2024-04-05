de0223cb24661c4e22c2b0ed15878d52afd6dc48
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-05 10:08:00 -0300

    adjusting viewmodel🧛

---
+++ src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure_by_id/app/GetBloodPressureByIdViewmodel.java
@@ -0,0 +1,19 @@
+
+package com.maua.yegestaodesaude.modules.bloodPressure.get_blood_pressure_by_id.app;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class GetBloodPressureByIdViewmodel {
+
+    private Date date;
+    private String measure;
+    private String level;
+
+}
