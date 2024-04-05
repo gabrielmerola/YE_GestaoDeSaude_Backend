0b881e643977b5205dc449cd9c9bbf56a4e0bea3
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-05 10:07:52 -0300

    adjusting usecase🧛

--- src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure_by_id/app/GetBloodPressureByIdUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure_by_id/app/GetBloodPressureByIdUsecase.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.maua.yegestaodesaude.modules.bloodPressure.get_blood_pressure_by_id.GetBloodPressureByIdViewmodel;
 import com.maua.yegestaodesaude.shared.domain.entities.BloodPressure;
 import com.maua.yegestaodesaude.shared.domain.repositories.BloodPressureRepository;
 
