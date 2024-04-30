f2d0528402a324ad61cc6341cc7663e45362dc75
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-30 12:17:34 -0300

    Refactor GetAllLaboratoryTestUsecase to use autowired dependency and add endpoint for getting all laboratory tests

--- src/main/java/com/maua/yegestaodesaude/modules/laboratoryTest/get_all_laboratory_test/app/GetAllLaboratoryTestUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/laboratoryTest/get_all_laboratory_test/app/GetAllLaboratoryTestUsecase.java
@@ -1,5 +1,39 @@
 package com.maua.yegestaodesaude.modules.laboratoryTest.get_all_laboratory_test.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.maua.yegestaodesaude.shared.domain.entities.LaboratoryTest;
+
+import com.maua.yegestaodesaude.shared.domain.repositories.LaboratoryTestRepository;
+
+@Service
 public class GetAllLaboratoryTestUsecase {
     
+    @Autowired
+    private LaboratoryTestRepository laboratoryTestRepository;
+
+    public ResponseEntity<Object> execute() {
+        List<LaboratoryTest> laboratoryTest = laboratoryTestRepository.findAll();
+
+        if (laboratoryTest.isEmpty()) {
+            return ResponseEntity.status(204).body("{\"message\": \"Não encontrado\"}");
+        }
+
+        List<GetAllLaboratoryTestViewmodel> laboratoryTestList = new ArrayList<>();
+
+        for (LaboratoryTest glt : laboratoryTest) {
+            laboratoryTestList.add(GetAllLaboratoryTestViewmodel.builder()
+                    .id(glt.getId())
+                    .textName(glt.getTextName())
+                    .rangeRefMin(glt.getRangeRefMin())
+                    .rangeRefMax(glt.getRangeRefMax())
+                    .unitOfMeasurement(glt.getUnitOfMeasurement())
+                    .build());
+        }
+        return ResponseEntity.status(200).body(laboratoryTestList);
+    }
 }
