8faf2df596d57fc0de06cdafdb2d5504d3d8fe6a
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-30 12:17:38 -0300

    Refactor GetAllLaboratoryTestViewmodel to include additional fields

--- src/main/java/com/maua/yegestaodesaude/modules/laboratoryTest/get_all_laboratory_test/app/GetAllLaboratoryTestViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/laboratoryTest/get_all_laboratory_test/app/GetAllLaboratoryTestViewmodel.java
@@ -1,5 +1,21 @@
 package com.maua.yegestaodesaude.modules.laboratoryTest.get_all_laboratory_test.app;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class GetAllLaboratoryTestViewmodel {
+
+    private Long id;
+    private String textName;
+    private String gender;
+    private int rangeRefMin;
+    private int rangeRefMax;
+    private String unitOfMeasurement;
     
 }
