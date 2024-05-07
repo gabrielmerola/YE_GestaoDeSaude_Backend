f2c20d23de0cc80467ec0378cd91e7955416c545
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-07 11:05:39 -0300

    Validation CreateConsultation

--- src/main/java/com/maua/yegestaodesaude/modules/consultation/create_consultation/app/CreateConsultationUsecase.java
+++ src/main/java/com/maua/yegestaodesaude/modules/consultation/create_consultation/app/CreateConsultationUsecase.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
-
 @Service
 public class CreateConsultationUsecase {
 
@@ -26,15 +24,37 @@ public class CreateConsultationUsecase {
                 throw new RuntimeException("Cliente não encontrado!");
             });
 
-            Date sqlDate = Date.valueOf(consultationDTO.date());
-            Date sqlDateReturn = Date.valueOf(consultationDTO.dateReturn());
+            if(consultationDTO.name().isEmpty() || consultationDTO.name() == null){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Nome da consulta não pode ser vazio"));
+            }
+            if(consultationDTO.expertise().isEmpty() || consultationDTO.expertise() == null){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Especialidade da consulta não pode ser vazio"));
+            }
+            if(consultationDTO.date().isEmpty() || consultationDTO.date() == null){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Data da consulta não pode ser vazio"));
+            }
+            if(consultationDTO.dateReturn().isEmpty() || consultationDTO.dateReturn() == null){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Data de retorno da consulta não pode ser vazio"));
+            }
+            if(consultationDTO.description().isEmpty() || consultationDTO.description() == null){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Descrição da consulta não pode ser vazio"));
+            }
+
+            String regexDate = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$";
+
+            if(!consultationDTO.date().matches(regexDate)){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Data da consulta inválida"));
+            }
+            if(!consultationDTO.dateReturn().matches(regexDate)){
+                return ResponseEntity.status(422).body(new CreateConsultationViewmodel("Data de retorno da consulta inválida"));
+            }
 
             Consultation consultation = new Consultation();
             consultation.setClient(client);
             consultation.setName(consultationDTO.name());
             consultation.setExpertise(consultationDTO.expertise());
-            consultation.setDate(sqlDate);
-            consultation.setDateReturn(sqlDateReturn);
+            consultation.setDate(consultationDTO.date());
+            consultation.setDateReturn(consultationDTO.dateReturn());
             consultation.setDescription(consultationDTO.description());
 
             consultation = consultationRepository.save(consultation);
