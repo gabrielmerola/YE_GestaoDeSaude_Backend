86318c369597361c694543ad3bf749043d52fe24
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-07 10:06:33 -0300

    adjusting GetBloodPressure

--- src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure_by_id/app/GetBloodPressureByIdViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/bloodPressure/get_blood_pressure_by_id/app/GetBloodPressureByIdViewmodel.java
@@ -1,8 +1,6 @@
 
 package com.maua.yegestaodesaude.modules.bloodPressure.get_blood_pressure_by_id.app;
 
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +10,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class GetBloodPressureByIdViewmodel {
 
-    private Date date;
+    private String date;
     private String measure;
     private String level;
 
