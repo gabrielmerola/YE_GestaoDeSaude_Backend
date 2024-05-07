c364c833b96f82709f0f6109da7242a2aef24b34
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-07 10:05:14 -0300

    Adding function FormatDate pt-BR

--- src/main/java/com/maua/yegestaodesaude/shared/utils/DateUtils.java
+++ src/main/java/com/maua/yegestaodesaude/shared/utils/DateUtils.java
@@ -4,10 +4,18 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DateUtils {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat DATE_FORMAT_REVERT = new SimpleDateFormat("dd/MM/yyyy");
 
     public static Date convertStringToDate(String dateString) throws ParseException {
         return DATE_FORMAT.parse(dateString);
+    }
+
+    public String RevertDate(Date date) {
+        return DATE_FORMAT_REVERT.format(date);
     }
 }
