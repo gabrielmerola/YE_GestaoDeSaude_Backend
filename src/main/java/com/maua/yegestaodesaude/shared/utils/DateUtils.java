075bd2ae5e8ed026628940619c4f068859f66d2b
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-07 11:03:36 -0300

    adding RevertDateString

--- src/main/java/com/maua/yegestaodesaude/shared/utils/DateUtils.java
+++ src/main/java/com/maua/yegestaodesaude/shared/utils/DateUtils.java
@@ -11,11 +11,20 @@ public class DateUtils {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private static final SimpleDateFormat DATE_FORMAT_REVERT = new SimpleDateFormat("dd/MM/yyyy");
 
-    public static Date convertStringToDate(String dateString) throws ParseException {
+    public Date convertStringToDate(String dateString) throws ParseException {
         return DATE_FORMAT.parse(dateString);
     }
 
     public String RevertDate(Date date) {
         return DATE_FORMAT_REVERT.format(date);
+    }
+
+    public String RevertDateString(String date) {
+        String regexDate = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$";
+
+        if(date.matches(regexDate)){
+            return date.split("-")[2] + "/" + date.split("-")[1] + "/" + date.split("-")[0];   
+        }
+        return "Data inválida";
     }
 }
