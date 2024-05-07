8c0c730aaf99358278b6a1356100205056c447c0
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-07 11:16:30 -0300

    ajusting GlucoseViewmodel

--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose_by_id/app/GetGlucoseByIdViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_glucose_by_id/app/GetGlucoseByIdViewmodel.java
@@ -1,7 +1,5 @@
 package com.maua.yegestaodesaude.modules.glucose.get_glucose_by_id.app;
 
-import java.sql.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +9,7 @@ import lombok.Data;
 @Builder
 public class GetGlucoseByIdViewmodel {
     private Long id;
-    private Date date;
+    private String date;
     private Integer measure;
     private String level;
 }
--- src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseViewmodel.java
+++ src/main/java/com/maua/yegestaodesaude/modules/glucose/get_latest_glucose/app/GetLatestGlucoseViewmodel.java
@@ -1,7 +1,5 @@
 package com.maua.yegestaodesaude.modules.glucose.get_latest_glucose.app;
 
-import java.sql.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +9,7 @@ import lombok.Data;
 @Builder
 public class GetLatestGlucoseViewmodel {
     private Long id;
-    private Date date;
+    private String date;
     private String measure;
     private String level;
 }
